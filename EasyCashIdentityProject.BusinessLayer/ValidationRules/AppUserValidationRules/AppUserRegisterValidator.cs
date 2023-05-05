d09332d63cd3aa5c126dac0ebd74833e5cd1d984
Samet Tuzuner <samettznerr@gmail.com>
2023-05-05 18:23:14 +0300

    RuleFor Add

---
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,26 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUsersDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator:AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad Alanı Boş Geçilemez");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad Alanı Boş Geçilemez");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanıcı Adı Alanı Boş Geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre Alanı Boş Geçilemez");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre Tekrar Alanı Boş Geçilemez");
+            RuleFor(x => x.Name).MaximumLength(20).WithMessage("Lütfen En Fazla 20 Karakter Giriniz");
+            RuleFor(x => x.Name).MinimumLength(2).WithMessage("Lütfen En Az 2 Karakter Giriniz");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("Şifreniz Birbiriyle Eşleşmiyor");
+
+        }
+    }
+}
